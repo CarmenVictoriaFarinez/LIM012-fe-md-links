7eba16d99fec6352c4c55b3ed8aa0ea845522f73
CarmenVictoriaFarinez <acuario1140@gmail.com>
2020-05-17 20:38:40 -0500

    clicasi

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 const process = require('process');
 const mdLinks = require('./index');
 const { getLinksStats, getBrokenLinksStats } = require('./options');
@@ -5,12 +6,12 @@ const { getLinksStats, getBrokenLinksStats } = require('./options');
 // extrayendo de la cadena los 2 argumentos primeros de la linea de comandos: node, nombrearchivo//
 const argv = process.argv.slice(2);
 const path = argv[0];
+const optionValidate = argv[1];
+const optionStats = argv[2];
 const options = {
   validate: false,
   stats: false,
 };
-const optionValidate = argv[1];
-const optionStats = argv[2];
 
 if (argv.length) {
   mdLinks(path, options)
@@ -25,14 +26,21 @@ if (argv.length) {
         console.log(`\n Path :${links.file} \n Link : ${links.href}  ${links.status}  ${links.message} 
       \n texto : ${links.text} `);
       }));
+  } else if (optionStats === '--stats') {
+    options.stats = true;
+    getLinksStats(path)
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
   }
   if (optionValidate === '--validate' && optionStats === '--stats') {
     options.validate = true;
     options.stats = true;
     Promise.all([getLinksStats(path), getBrokenLinksStats(path)])
-      .then((res) => res.forEach((properties) => console.log(properties)))
+      .then((response) => response.forEach((properties) => console.log(properties)))
       .catch((error) => console.log(error));
   }
 }
 
 mdLinks('./test/test-API/test.md', { stats: true }).then((res) => console.log(res));
+// getLinksStats('./test/test-API/test.md').then((res) => console.log(res));
+// getBrokenLinksStats('./test/test-API/test.md').then((res) => console.log(res));
